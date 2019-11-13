f8fe0e1a685bb2151b5717b2a3e94e5ba18dde06
dugbraden <36244464+dugbraden@users.noreply.github.com>
2019-11-12 21:36:34 -0500

    Update soil.ts
    changed the moisture scale to reflect empirical values, modified handler to single criteria (less than instead of between), decapitalized, modified some language

--- soil.ts
+++ soil.ts
@@ -1,10 +1,10 @@
 enum Mlevel {
     //% block="Very Wet"
-    VERY_WET = 750,
+    VERY_WET = 500,
     //% block="Wet"
-    WET = 500,
+    WET = 200,
     //% block="Dry"
-    DRY = 250,
+    DRY = 100,
     //% block="Very Dry"
     VERY_DRY = 0
 }
@@ -14,37 +14,32 @@ namespace soil {
     /**
      * Returns the value of the moisture sensor at a specific pin.
      */
-    //% block="If moisture level at pin $pin is ____"
+    //% block="moisture level at pin $pin"
     export function getMoisture(pin: AnalogPin): number {
         return pins.analogReadPin(pin)
     }
 
 
     /**
-     * Runs a specific function if the moisture value is between the two passed moisture levels
+     * Runs a specific function if the moisture value is below the selected moisture level
      */
-    //% block="If moisture level at $pin is between $mlevel and $mlevel2"
+    //% block="on moisture level at $pin is below $mlevel"
     export function ifMoisture(pin: AnalogPin, mlevel: Mlevel, mlevel2: Mlevel, handler: () => void) {
         let moistureLevel = getMoisture(pin)
-        if (moistureLevel >= mlevel) {
-            if (moistureLevel <= mlevel2) {
+        if (moistureLevel <= mlevel) {
                 handler()
-            }
         }
-        else if (moistureLevel >= mlevel2) {
-            if (moistureLevel <= mlevel) {
-                handler()
-            }
-        }
+       
+        
     }
 
     /**
-     * Displays the read moisture level on the LED grid
+     * Displays the read moisture level on a vertical bar graph up to full scale.
      */
-    //% block="moisture at pin %pin to LED"
+    //% block="display moisture level at %pin"
     //% weight=40
     export function displayMoisture(pin: AnalogPin): void {
-        led.plotBarGraph(getMoisture(pin), 1023)
+        led.plotBarGraph(getMoisture(pin), 800)
     }
 
 }
